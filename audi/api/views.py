81fff5896f1b87cd7d48a28ff97af9bb0d4df732
Igor-Yudin <Igor_Yudin@inbox.ru>
2017-11-26 18:26:09 +0300

    Authenticated permission

--- audi/api/views.py
+++ audi/api/views.py
@@ -18,22 +18,24 @@ from api.permissions import *
 class JobTypeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = JobType.objects.all()
     serializer_class = JobTypeSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class MaterialViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Material.objects.all()
     serializer_class = MaterialSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class UsedMaterialViewSet(viewsets.ModelViewSet):
     queryset = UsedMaterial.objects.all()
     serializer_class = UsedMaterialSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticated,)
 
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticated,)
 
     def perform_create(self, serializer):
         serializer.save(inspector=self.request.user)
